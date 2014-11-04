a80a8f317095853a886e38895bb96dcc70c67348
Daniel X Moore <yahivin@gmail.com>
2014-11-03 20:13:14 -0800

    :negative_squared_cross_mark::jp: Updated in browser at strd6.github.io/editor

--- docs/package.js
+++ docs/package.js
@@ -186,13 +186,13 @@
     },
     "data_loader.coffee.md": {
       "path": "data_loader.coffee.md",
-      "content": "Data Loader\n===========\n\n    Spreadsheet = require \"spreadsheet\"\n    sheetData = null\n\n    loader = null\n    get = ->\n      return loader if loader\n\n      loader = Spreadsheet.load(\"0ArtCBkZR37MmdFJqbjloVEp1OFZLWDJ6M29OcXQ1WkE\")\n\n      return loader\n\n    module.exports =\n      characters: ->\n        get().then (data) ->\n          characterFromRemote(data.Characters)\n      names: ->\n        get().then (data) ->\n          data.Names.map (row) ->\n            name: row.name.trim()\n            gender: row.gender.trim()\n            culture: row.culture.trim()\n      get: get\n\n    characterDataTransform = (data) ->\n      extend data,\n        healthMax: data.healthmax\n        abilities: data.abilities.split(',')\n        passives: (data.passives ? \"\").split(',')\n        spriteName: data.sprite\n\n      delete data.healthmax\n      delete data.sprite\n\n      return data\n\n    characterFromRemote = (data) ->\n      console.log data\n      results = {}\n      data.forEach (datum) ->\n        results[datum.name] = characterDataTransform(datum)\n\n      return results\n",
+      "content": "Data Loader\n===========\n\n    Spreadsheet = require \"spreadsheet\"\n    sheetData = null\n\n    loader = null\n    get = ->\n      return loader if loader\n\n      loader = Spreadsheet.load(\"0ArtCBkZR37MmdFJqbjloVEp1OFZLWDJ6M29OcXQ1WkE\")\n\n      return loader\n\n    module.exports =\n      characters: ->\n        get().then (data) ->\n          characterFromRemote(data.characters)\n      names: ->\n        get().then (data) ->\n          data.names.map (row) ->\n            name: row.name.trim()\n            gender: row.gender.trim()\n            culture: row.culture.trim()\n      get: get\n\n    characterDataTransform = (data) ->\n      extend data,\n        healthMax: data.healthmax\n        abilities: data.abilities.split(',')\n        passives: (data.passives ? \"\").split(',')\n        spriteName: data.sprite\n\n      delete data.healthmax\n      delete data.sprite\n\n      return data\n\n    characterFromRemote = (data) ->\n      console.log data\n      results = {}\n      data.forEach (datum) ->\n        results[datum.name] = characterDataTransform(datum)\n\n      return results\n",
       "mode": "100644",
       "type": "blob"
     },
     "lib/engine.coffee.md": {
       "path": "lib/engine.coffee.md",
-      "content": "Engine\n======\n \n    require \"cornerstone\"\n\n    module.exports = (I={}, self={}) ->\n      defaults I,\n        dt: 1/60\n        t: 0\n        paused: false\n        running: false\n\n      step = ->\n        unless I.paused\n          self.update?(I.t, I.dt)\n\n        self.render?(I.t, I.dt)\n\n      animLoop = (timestamp) ->\n        step()\n\n        if I.running\n          window.requestAnimationFrame(animLoop)\n\n      if I.running\n        window.requestAnimationFrame(animLoop)\n\n      extend self,\n        start: ->\n          unless I.running\n            I.running = true\n            animLoop()\n\n        stop: ->\n          I.running = false\n",
+      "content": "Engine\n======\n \n    require \"cornerstone\"\n\n    module.exports = (I={}, self={}) ->\n      defaults I,\n        dt: 1/60\n        t: 0\n        paused: false\n        running: false\n\n      step = ->\n        unless I.paused\n          self.update?(I.t, I.dt)\n          I.t += I.dt\n\n        self.render?(I.t, I.dt)\n\n      animLoop = (timestamp) ->\n        step()\n\n        if I.running\n          window.requestAnimationFrame(animLoop)\n\n      if I.running\n        window.requestAnimationFrame(animLoop)\n\n      extend self,\n        start: ->\n          unless I.running\n            I.running = true\n            animLoop()\n\n        stop: ->\n          I.running = false\n",
       "mode": "100644",
       "type": "blob"
     },
@@ -216,7 +216,7 @@
     },
     "pixie.cson": {
       "path": "pixie.cson",
-      "content": "version: \"0.2.1\"\nentryPoint: \"main\"\nremoteDependencies: [\n  \"https://code.jquery.com/jquery-1.10.1.min.js\"\n  \"https://cdnjs.cloudflare.com/ajax/libs/three.js/r69/three.min.js\"\n]\ndependencies:\n  cornerstone: \"distri/cornerstone:v0.2.6\"\n  spreadsheet: \"distri/gdocs-spreadsheet:v0.1.0\"\n  stats: \"distri/stats.js:v0.11.0\"\n  util: \"distri/util:v0.1.0\"\n",
+      "content": "version: \"0.2.2\"\nentryPoint: \"main\"\nremoteDependencies: [\n  \"https://code.jquery.com/jquery-1.10.1.min.js\"\n  \"https://cdnjs.cloudflare.com/ajax/libs/three.js/r69/three.min.js\"\n]\ndependencies:\n  cornerstone: \"distri/cornerstone:v0.2.6\"\n  spreadsheet: \"distri/gdocs-spreadsheet:v0.1.0\"\n  stats: \"distri/stats.js:v0.11.0\"\n  util: \"distri/util:v0.1.0\"\n",
       "mode": "100644",
       "type": "blob"
     },
@@ -228,7 +228,7 @@
     },
     "test/engine.coffee": {
       "path": "test/engine.coffee",
-      "content": "Engine = require \"../lib/engine\"\n\ndescribe \"engine\", ->\n  it \"should start and stop\", (done) ->\n    engine = Engine({},\n      update: ->\n        engine.stop()\n        done()\n    )\n\n    engine.start()\n\n  it \"should update about 60 times a second\", (done) ->\n    c = 0\n\n    engine = Engine {},\n      update: ->\n        c += 1\n\n    engine.start()\n\n    setTimeout ->\n      console.log c\n      assert c > 58\n      assert c < 62\n      engine.stop()\n      done()\n    , 1000\n",
+      "content": "Engine = require \"../lib/engine\"\n\nequalEnough = (a, b) ->\n  assert (b - a) < 0.0001\n  assert (a - b) < 0.0001\n\ndescribe \"engine\", ->\n  it \"should start and stop\", (done) ->\n    engine = Engine({},\n      update: ->\n        engine.stop()\n        done()\n    )\n\n    engine.start()\n\n  it \"should update about 60 times a second\", (done) ->\n    c = 0\n\n    engine = Engine {},\n      update: (t, dt) ->\n        equalEnough(t, c * 1/60)\n        c += 1\n\n        assert.equal dt, 1/60\n\n    engine.start()\n\n    setTimeout ->\n      console.log c\n      assert c > 58\n      assert c < 62\n      engine.stop()\n      done()\n    , 1000\n",
       "mode": "100644",
       "type": "blob"
     },
@@ -284,12 +284,12 @@
     },
     "data_loader": {
       "path": "data_loader",
-      "content": "(function() {\n  var Spreadsheet, characterDataTransform, characterFromRemote, get, loader, sheetData;\n\n  Spreadsheet = require(\"spreadsheet\");\n\n  sheetData = null;\n\n  loader = null;\n\n  get = function() {\n    if (loader) {\n      return loader;\n    }\n    loader = Spreadsheet.load(\"0ArtCBkZR37MmdFJqbjloVEp1OFZLWDJ6M29OcXQ1WkE\");\n    return loader;\n  };\n\n  module.exports = {\n    characters: function() {\n      return get().then(function(data) {\n        return characterFromRemote(data.Characters);\n      });\n    },\n    names: function() {\n      return get().then(function(data) {\n        return data.Names.map(function(row) {\n          return {\n            name: row.name.trim(),\n            gender: row.gender.trim(),\n            culture: row.culture.trim()\n          };\n        });\n      });\n    },\n    get: get\n  };\n\n  characterDataTransform = function(data) {\n    var _ref;\n    extend(data, {\n      healthMax: data.healthmax,\n      abilities: data.abilities.split(','),\n      passives: ((_ref = data.passives) != null ? _ref : \"\").split(','),\n      spriteName: data.sprite\n    });\n    delete data.healthmax;\n    delete data.sprite;\n    return data;\n  };\n\n  characterFromRemote = function(data) {\n    var results;\n    console.log(data);\n    results = {};\n    data.forEach(function(datum) {\n      return results[datum.name] = characterDataTransform(datum);\n    });\n    return results;\n  };\n\n}).call(this);\n",
+      "content": "(function() {\n  var Spreadsheet, characterDataTransform, characterFromRemote, get, loader, sheetData;\n\n  Spreadsheet = require(\"spreadsheet\");\n\n  sheetData = null;\n\n  loader = null;\n\n  get = function() {\n    if (loader) {\n      return loader;\n    }\n    loader = Spreadsheet.load(\"0ArtCBkZR37MmdFJqbjloVEp1OFZLWDJ6M29OcXQ1WkE\");\n    return loader;\n  };\n\n  module.exports = {\n    characters: function() {\n      return get().then(function(data) {\n        return characterFromRemote(data.characters);\n      });\n    },\n    names: function() {\n      return get().then(function(data) {\n        return data.names.map(function(row) {\n          return {\n            name: row.name.trim(),\n            gender: row.gender.trim(),\n            culture: row.culture.trim()\n          };\n        });\n      });\n    },\n    get: get\n  };\n\n  characterDataTransform = function(data) {\n    var _ref;\n    extend(data, {\n      healthMax: data.healthmax,\n      abilities: data.abilities.split(','),\n      passives: ((_ref = data.passives) != null ? _ref : \"\").split(','),\n      spriteName: data.sprite\n    });\n    delete data.healthmax;\n    delete data.sprite;\n    return data;\n  };\n\n  characterFromRemote = function(data) {\n    var results;\n    console.log(data);\n    results = {};\n    data.forEach(function(datum) {\n      return results[datum.name] = characterDataTransform(datum);\n    });\n    return results;\n  };\n\n}).call(this);\n",
       "type": "blob"
     },
     "lib/engine": {
       "path": "lib/engine",
-      "content": "(function() {\n  require(\"cornerstone\");\n\n  module.exports = function(I, self) {\n    var animLoop, step;\n    if (I == null) {\n      I = {};\n    }\n    if (self == null) {\n      self = {};\n    }\n    defaults(I, {\n      dt: 1 / 60,\n      t: 0,\n      paused: false,\n      running: false\n    });\n    step = function() {\n      if (!I.paused) {\n        if (typeof self.update === \"function\") {\n          self.update(I.t, I.dt);\n        }\n      }\n      return typeof self.render === \"function\" ? self.render(I.t, I.dt) : void 0;\n    };\n    animLoop = function(timestamp) {\n      step();\n      if (I.running) {\n        return window.requestAnimationFrame(animLoop);\n      }\n    };\n    if (I.running) {\n      window.requestAnimationFrame(animLoop);\n    }\n    return extend(self, {\n      start: function() {\n        if (!I.running) {\n          I.running = true;\n          return animLoop();\n        }\n      },\n      stop: function() {\n        return I.running = false;\n      }\n    });\n  };\n\n}).call(this);\n",
+      "content": "(function() {\n  require(\"cornerstone\");\n\n  module.exports = function(I, self) {\n    var animLoop, step;\n    if (I == null) {\n      I = {};\n    }\n    if (self == null) {\n      self = {};\n    }\n    defaults(I, {\n      dt: 1 / 60,\n      t: 0,\n      paused: false,\n      running: false\n    });\n    step = function() {\n      if (!I.paused) {\n        if (typeof self.update === \"function\") {\n          self.update(I.t, I.dt);\n        }\n        I.t += I.dt;\n      }\n      return typeof self.render === \"function\" ? self.render(I.t, I.dt) : void 0;\n    };\n    animLoop = function(timestamp) {\n      step();\n      if (I.running) {\n        return window.requestAnimationFrame(animLoop);\n      }\n    };\n    if (I.running) {\n      window.requestAnimationFrame(animLoop);\n    }\n    return extend(self, {\n      start: function() {\n        if (!I.running) {\n          I.running = true;\n          return animLoop();\n        }\n      },\n      stop: function() {\n        return I.running = false;\n      }\n    });\n  };\n\n}).call(this);\n",
       "type": "blob"
     },
     "lib/extensions": {
@@ -309,7 +309,7 @@
     },
     "pixie": {
       "path": "pixie",
-      "content": "module.exports = {\"version\":\"0.2.1\",\"entryPoint\":\"main\",\"remoteDependencies\":[\"https://code.jquery.com/jquery-1.10.1.min.js\",\"https://cdnjs.cloudflare.com/ajax/libs/three.js/r69/three.min.js\"],\"dependencies\":{\"cornerstone\":\"distri/cornerstone:v0.2.6\",\"spreadsheet\":\"distri/gdocs-spreadsheet:v0.1.0\",\"stats\":\"distri/stats.js:v0.11.0\",\"util\":\"distri/util:v0.1.0\"}};",
+      "content": "module.exports = {\"version\":\"0.2.2\",\"entryPoint\":\"main\",\"remoteDependencies\":[\"https://code.jquery.com/jquery-1.10.1.min.js\",\"https://cdnjs.cloudflare.com/ajax/libs/three.js/r69/three.min.js\"],\"dependencies\":{\"cornerstone\":\"distri/cornerstone:v0.2.6\",\"spreadsheet\":\"distri/gdocs-spreadsheet:v0.1.0\",\"stats\":\"distri/stats.js:v0.11.0\",\"util\":\"distri/util:v0.1.0\"}};",
       "type": "blob"
     },
     "test/character": {
@@ -319,7 +319,7 @@
     },
     "test/engine": {
       "path": "test/engine",
-      "content": "(function() {\n  var Engine;\n\n  Engine = require(\"../lib/engine\");\n\n  describe(\"engine\", function() {\n    it(\"should start and stop\", function(done) {\n      var engine;\n      engine = Engine({}, {\n        update: function() {\n          engine.stop();\n          return done();\n        }\n      });\n      return engine.start();\n    });\n    return it(\"should update about 60 times a second\", function(done) {\n      var c, engine;\n      c = 0;\n      engine = Engine({}, {\n        update: function() {\n          return c += 1;\n        }\n      });\n      engine.start();\n      return setTimeout(function() {\n        console.log(c);\n        assert(c > 58);\n        assert(c < 62);\n        engine.stop();\n        return done();\n      }, 1000);\n    });\n  });\n\n}).call(this);\n",
+      "content": "(function() {\n  var Engine, equalEnough;\n\n  Engine = require(\"../lib/engine\");\n\n  equalEnough = function(a, b) {\n    assert((b - a) < 0.0001);\n    return assert((a - b) < 0.0001);\n  };\n\n  describe(\"engine\", function() {\n    it(\"should start and stop\", function(done) {\n      var engine;\n      engine = Engine({}, {\n        update: function() {\n          engine.stop();\n          return done();\n        }\n      });\n      return engine.start();\n    });\n    return it(\"should update about 60 times a second\", function(done) {\n      var c, engine;\n      c = 0;\n      engine = Engine({}, {\n        update: function(t, dt) {\n          equalEnough(t, c * 1 / 60);\n          c += 1;\n          return assert.equal(dt, 1 / 60);\n        }\n      });\n      engine.start();\n      return setTimeout(function() {\n        console.log(c);\n        assert(c > 58);\n        assert(c < 62);\n        engine.stop();\n        return done();\n      }, 1000);\n    });\n  });\n\n}).call(this);\n",
       "type": "blob"
     },
     "test/loading": {
@@ -366,7 +366,7 @@
   "progenitor": {
     "url": "http://www.danielx.net/editor/"
   },
-  "version": "0.2.1",
+  "version": "0.2.2",
   "entryPoint": "main",
   "remoteDependencies": [
     "https://code.jquery.com/jquery-1.10.1.min.js",
